38c8a649f0bb4804afe60678dfbf1510db80971f
Alzhan <turlyalz@fit.cvut.cz>
2015-10-27 19:53:04 +0100

    ch

--- KOSeek/LoginViewController.swift
+++ KOSeek/LoginViewController.swift
@@ -43,7 +43,6 @@ class LoginViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
