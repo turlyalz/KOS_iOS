b8edb4f59f17292e1b20c9d82e5fbad74e14a574
Alzhan <turlyalz@fit.cvut.cz>
2015-10-30 18:50:09 +0100

    saved variables class

---
+++ KOSeek/SavedVariables.swift
@@ -0,0 +1,17 @@
+//
+//  SavedVariables.swift
+//  KOSeek
+//
+//  Created by Alzhan on 30.10.15.
+//  Copyright © 2015 Alzhan Turlybekov. All rights reserved.
+//
+
+import Foundation
+
+
+class SavedVariables {
+    static var sideMenuViewController: SideMenuController?
+    static var username: String?
+    
+    private init(){ }
+}
