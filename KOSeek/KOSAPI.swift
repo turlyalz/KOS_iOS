76ef0e5eac5c91089b5be5b2d33c6a4eefe95fc6
Alzhan <turlyalz@fit.cvut.cz>
2015-11-11 18:57:26 +0100

    Database

---
+++ KOSeek/KOSAPI.swift
@@ -0,0 +1,15 @@
+//
+//  KOSAPI.swift
+//  KOSeek
+//
+//  Created by Alzhan on 11.11.15.
+//  Copyright © 2015 Alzhan Turlybekov. All rights reserved.
+//
+
+import Foundation
+
+
+class KOSAPI {
+    
+    private init(){ }
+}
