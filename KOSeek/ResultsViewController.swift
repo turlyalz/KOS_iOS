89c43db1600ba8fce98b9e75b234a38bd33eb5be
Alzhan <turlyalz@fit.cvut.cz>
2015-11-20 00:52:39 +0100

    dd

--- KOSeek/ResultsViewController.swift
+++ KOSeek/ResultsViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ResultsViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ResultsViewController: UITableViewController {
 
-    @IBOutlet weak var subjectsTableView: UITableView!
-    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var menuButton: UIBarButtonItem!
-    var pickerData: [(String, String)] = []
-    var subjectsTableViewDelegate: SubjectsTableViewDelegate?
+    var semesterIDNameDict: [(String, String)] = []
+    var semesters: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +22,18 @@ class ResultsViewController: UITableViewController, UIPickerViewDataSource, UIPi
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
         
-        subjectsTableViewDelegate = subjectsTableView as? SubjectsTableViewDelegate
-        subjectsTableView.dataSource = subjectsTableViewDelegate
-        subjectsTableView.delegate = subjectsTableViewDelegate
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        pickerData = SavedVariables.semesterIDNameDict.sort({$0.0 < $1.0})
-        subjectsTableViewDelegate?.updateActualSubjects(pickerData[0].0)
+        semesterIDNameDict = SavedVariables.semesterIDNameDict.sort({$0.0 < $1.0})
+        for (_, semester) in semesterIDNameDict {
+            semesters.append(semester)
+        }
         
-        let items = Array(SavedVariables.semesterIDNameDict.values)
-        let menuView = BTNavigationDropdownMenu(title: items.first!, items: items, navController: self.navigationController)
-        
+        setupDropdownMenu()
+        let firstSemester = semesterIDNameDict.first!
+        updateActualSubjects(firstSemester.0)
+    }
+    
+    func setupDropdownMenu() {
+        let menuView = BTNavigationDropdownMenu(title: semesters.first!, items: semesters, navController: self.navigationController)
         menuView.cellHeight = 35
         menuView.cellBackgroundColor =  UIColor(red: 120/255, green: 162/255, blue: 182/255, alpha: 1.0)
         menuView.cellSelectionColor = UIColor(red: 110/255, green: 115/255, blue: 120/255, alpha: 1.0)
@@ -45,7 +44,8 @@ class ResultsViewController: UITableViewController, UIPickerViewDataSource, UIPi
         menuView.maskBackgroundOpacity = 0.3
         menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
-            //self.selectedCellLabel.text = items[indexPath]
+            let semester = self.semesterIDNameDict[indexPath].0
+            self.updateActualSubjects(semester)
         }
         self.navigationItem.titleView = menuView
     }
@@ -56,38 +56,60 @@ class ResultsViewController: UITableViewController, UIPickerViewDataSource, UIPi
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    //MARK: - Delegates and data sources for picker view
-    //MARK: Data Sources
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    
+    var actualSubjects: [Subject] = []
+    
+    func updateActualSubjects(semester: String) {
+        if let subj = Database.getSubjectsBy(semester: semester) {
+            //print(subj)
+            actualSubjects = subj
+            actualSubjects.sortInPlace({ $0.0.code < $0.1.code })
+            tableView.reloadData()
+        }
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
-    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
-        var pickerLabel = view as? UILabel
-        if pickerLabel == nil {
-            pickerLabel = UILabel()
-            pickerLabel?.font = UIFont(name: "Montserrat", size: 18)
-            pickerLabel?.textAlignment = NSTextAlignment.Center
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return actualSubjects.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let subject = actualSubjects[indexPath.row]
+        
+        let subjectCodeLabel: UILabel = UILabel(frame: CGRect(x: 10, y: 0, width: SubjectCell.subjectCodeWidth, height: SubjectCell.height))
+        subjectCodeLabel.text = subject.code
+        subjectCodeLabel.numberOfLines = 2
+        subjectCodeLabel.font = .systemFontOfSize(14)
+        subjectCodeLabel.textColor = .blackColor()
+        cell.addSubview(subjectCodeLabel)
+        
+        let subjectNameLabel: UILabel = UILabel(frame: CGRect(x: SubjectCell.subjectCodeWidth+7, y: 0, width: SubjectCell.subjectNameWidth, height: SubjectCell.height))
+        subjectNameLabel.text = subject.name
+        subjectNameLabel.numberOfLines = 2
+        subjectNameLabel.font = .systemFontOfSize(15)
+        subjectNameLabel.textColor = .blackColor()
+        subjectNameLabel.textAlignment = NSTextAlignment.Center
+        cell.addSubview(subjectNameLabel)
+        
+        let subjectCreditsLabel: UILabel = UILabel(frame: CGRect(x: SubjectCell.subjectCodeWidth+SubjectCell.subjectNameWidth+7, y: 0, width: SubjectCell.subjectCreditWidth, height: SubjectCell.height))
+        subjectCreditsLabel.text = subject.credits
+        subjectCreditsLabel.font = .systemFontOfSize(15)
+        subjectCreditsLabel.textColor = .blackColor()
+        subjectCreditsLabel.textAlignment = NSTextAlignment.Center
+        cell.addSubview(subjectCreditsLabel)
+        
+        if subject.completed == 1 {
+            cell.backgroundColor = UIColor(red: 155/255, green: 200/255, blue: 201/255, alpha: 1)
         }
-        pickerLabel?.text = pickerData[row].1
-        return pickerLabel!
+        
+        return cell
     }
     
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerData.count
-    }
-    
-    //MARK: Delegates
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        subjectsTableViewDelegate?.updateActualSubjects(pickerData[row].0)
-    }
-    
-    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-        updateValues()
-        self.subjectsTableViewDelegate?.reloadData()
-    }
 }
 
