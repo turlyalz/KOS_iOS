ff3800ab095843264ea9215902af82e9b1f62423
Alzhan <turlyalz@fit.cvut.cz>
2016-02-14 22:42:18 +0100

    delete exams

---
+++ KOSeek/src/PresentationLayer/CourseEventsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CourseEventsViewController.swift
+//  KOSeek
+//
+//  Created by Alzhan on 14.02.16.
+//  Copyright © 2016 Alzhan Turlybekov. All rights reserved.
+//
+
+import Cocoa
+
+class CourseEventsViewController: MainTableViewController {
+
+}
---
+++ KOSeek/src/PresentationLayer/RefreshControlProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  RefreshControlProtocol.swift
+//  KOSeek
+//
+//  Created by Alzhan on 10.02.16.
+//  Copyright © 2016 Alzhan Turlybekov. All rights reserved.
+//
+
+import Foundation
